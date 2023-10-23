a74de6602d20e9b0c44b4797e68ccc7889a26cee
wpertsch <willy.pertsch@gmail.com>
2023-10-23 12:05:47 +0200

    fuzzy paths

--- colbert/training/utils.py
+++ colbert/training/utils.py
@@ -31,12 +31,12 @@ def manage_checkpoints(args, colbert, optimizer, batch_idx, savepath=None, consu
     path_save = None
 
     if consumed_all_triples or (batch_idx % 2000 == 0):
-        name = os.path.join(path, "colbert.dnn")
+        name = os.path.join(checkpoints_path, "colbert.dnn")
         save_checkpoint(name, 0, batch_idx, colbert, optimizer, arguments)
         path_save = os.path.join(checkpoints_path, "colbert")
 
     if batch_idx in SAVED_CHECKPOINTS:
-        name = os.path.join(path, "colbert-{}.dnn".format(batch_idx))
+        name = os.path.join(checkpoints_path, "colbert-{}.dnn".format(batch_idx))
         save_checkpoint(name, 0, batch_idx, colbert, optimizer, arguments)
         path_save = os.path.join(checkpoints_path, f"colbert-{batch_idx}")
 
