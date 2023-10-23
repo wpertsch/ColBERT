820829beb2a3cc3d853422d2e01b07eed22f4ec5
wpertsch <willy.pertsch@gmail.com>
2023-10-23 18:34:13 +0200

    it is bad to commit so much, but it does not work otherwise

--- colbert/training/utils.py
+++ colbert/training/utils.py
@@ -13,7 +13,7 @@ def print_progress(scores):
 
 
 def manage_checkpoints(args, colbert, optimizer, batch_idx, savepath=None, consumed_all_triples=False):
-    # arguments = dict(args)
+    arguments = dict(args)
 
     # TODO: Call provenance() on the values that support it??
 
@@ -37,6 +37,7 @@ def manage_checkpoints(args, colbert, optimizer, batch_idx, savepath=None, consu
 
     if batch_idx in SAVED_CHECKPOINTS:
         name = os.path.join(checkpoints_path, "colbert-{}.dnn".format(batch_idx))
+        print(f"name is {name}")
         save_checkpoint(name, 0, batch_idx, colbert, optimizer, arguments)
         path_save = os.path.join(checkpoints_path, f"colbert-{batch_idx}")
 
